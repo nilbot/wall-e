6ac4f3c2603e8aaa02036662e2252b84fc0a5116
nilbot <ersi.ni@nilbot.net>
2012-08-12 14:54:43 +0200

    test windows only

--- hosts.go
+++ hosts.go
@@ -1,20 +1,27 @@
 // hosts
+
 package main
 
 import (
-	"log"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
 	"regexp"
 )
 
+var platform string
 var checkBegin, _ = regexp.Compile("<pre>")
 var checkEnd, _ = regexp.Compile("</pre>")
 
+func detectPlatform() string {
+	//not implemented, fake windows for now
+	return "windows"
+}
 func main() {
-
+	//starting, prompt something
+	fmt.Println("Doing 事儿")
 	resp, err := http.Get("http://goo.gl/hbhlU")
 	if err != nil {
 		log.Fatal(err)
@@ -25,7 +32,7 @@ func main() {
 		log.Fatal(err)
 	}
 
-	test, _ := os.Create("test")
+	test, _ := os.Create("resp.Body")
 	defer test.Close()
 	test.Write(body)
 	//this section checks hosts info embedded within
@@ -38,8 +45,23 @@ func main() {
 
 	hosts := body[iBegin:iEnd]
 	//test hosts
-	file, err := os.Create("hTest")
+	file, err := os.Create("parsed.Content")
 	defer file.Close()
 	file.Write(hosts)
 
+	if platform != "nix" {
+		syshosts, err := os.Open("%systemroot%/drivers/etc/hosts")
+		if err != nil {
+			log.Fatal(err)
+		}
+		syshosts.Seek(1, 2)
+		syshosts.Write(hosts)
+	} else {
+		syshosts, err := os.Open("/etc/hosts")
+		if err != nil {
+			log.Fatal(err)
+		}
+		syshosts.Seek(1, 2)
+		syshosts.Write(hosts)
+	}
 }
